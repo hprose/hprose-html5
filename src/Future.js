8cb5c3362ad64ce1edc27218edd46293127cfd6d
andot <mabingyao@gmail.com>
2015-05-04 19:39:07 +0800

    simplify future implementation

--- src/Future.js
+++ src/Future.js
@@ -24,78 +24,80 @@
     global.hprose = global.hprose || Object.create(null);
 
     global.hprose.Completer = function Completer() {
-        var callbacks = [];
-        var errorCallback = null;
-        var results = [];
-        var errors = [];
-        var future = Object.create(null);
+        var m_results = [];
+        var m_callbacks = [];
+        var m_error = null;
+        var m_onerror = null;
+        var m_future = Object.create(null);
 
+        // Calling complete or completeError must not be done more than once.
         function complete(result) {
-            results.push(result);
-            if (callbacks.length > 0) {
-                for (var i in callbacks) {
-                    var handler = callbacks[i];
-                    var result;
-                    for (var j in results) {
-                        try {
-                            result = handler(results[j]);
-                        }
-                        catch (e) {
-                            completeError(e);
-                        }
-                    }
-                    results = [result];
-                }
-                callbacks = [];
-            }
-        }
-
-        function completeError(error) {
-            if (errorCallback) {
-                errorCallback(error);
-            }
-            else {
-                errors.push(error);
-            }
-        }
-
-        function then(handler) {
-            if (results.length > 0) {
-                var result;
-                for (var i in results) {
+            m_results[0] = result;
+            if (m_callbacks.length > 0) {
+                for (var i in m_callbacks) {
                     try {
-                        result = handler(results[i]);
+                        var callback = m_callbacks[i];
+                        m_results[0] = callback(m_results[0]);
                     }
                     catch (e) {
-                        completeError(e);
+                        if (m_onerror !== null) {
+                            m_onerror(e);
+                        }
+                        else {
+                            m_error = e;
+                        }
                     }
                 }
-                results = [result];
+                m_callbacks.length = 0;
+            }
+        }
+
+        function completeError(e) {
+            if (m_onerror !== null) {
+                m_onerror(e);
             }
             else {
-                callbacks.push(handler);
+                m_error = e;
             }
-            return future;
         }
 
-        function catchError(errorHandler) {
-            errorCallback = errorHandler;
-            if (errors.length > 0) {
-                for (var i in errors) {
-                    errorCallback(errors[i]);
+        function then(callback) {
+            if (m_results.length > 0) {
+                try {
+                    m_results[0] = callback(m_results[0]);
                 }
-                errors = [];
+                catch (e) {
+                    if (m_onerror !== null) {
+                        m_onerror(e);
+                    }
+                    else {
+                        m_error = e;
+                    }
+                }
             }
-            return future;
+            else {
+                m_callbacks.push(callback);
+            }
+            return m_future;
         }
 
-        Object.defineProperties(future, {
+        function catchError(onerror) {
+            if (m_error !== null) {
+                onerror(m_error);
+            }
+            else {
+                m_onerror = onerror;
+            }
+            return m_future;
+        }
+
+        Object.defineProperties(m_future, {
             then: { value: then },
             catchError: { value: catchError },
         });
 
         Object.defineProperties(this, {
-            future: { get: function() { return future; } },
+            future: { get: function() { return m_future; } },
             complete: { value : complete },
             completeError: { value : completeError }
         });
